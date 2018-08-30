7c4b7fe1b4c0a51691c7e284683fc938c9743bbb
Lukegits <luket.chavez@gmail.com>
2018-08-30 16:30:42 -0600

    added landing page with login link that pulls data from google

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "82699793841-iaecj74m630m4amdqshngsanqbe7cih3.apps.googleusercontent.com", "OrNCHBUaKH6eGgA3neYaimo5"
+end
