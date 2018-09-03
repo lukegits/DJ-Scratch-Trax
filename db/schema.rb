50d4a337f24349d2eb912dc8fbdcaaaf5c5e8593
Lukegits <luket.chavez@gmail.com>
2018-09-03 10:53:52 -0600

    pg added

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_30_221623) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
