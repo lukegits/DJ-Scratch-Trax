4271a9c8149a5f8077325e8bf5f644e91256f1b7
Lukegits <luket.chavez@gmail.com>
2018-09-03 09:30:42 -0600

    heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+  get 'welcome/about'
   get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'auth/:provider/callback', to: 'sessions#create'
