a74ab8d19567bb3a6f506eba0e993bd58c157559
Lukegits <luket.chavez@gmail.com>
2018-09-13 16:30:15 -0600

    fixed bug in new topic button

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,6 @@ class TopicsController < ApplicationController
   # #7
   before_action :require_sign_in, except: [:index, :show]
 # #8
-  before_action :authorize_user, except: [:index, :show]
 
   def edit
     @topic = Topic.find(params[:id])
@@ -57,13 +56,4 @@ class TopicsController < ApplicationController
    def topic_params
      params.require(:topic).permit(:name, :description, :public)
    end
-
-
-# #9
-  def authorize_user
-    unless current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
-    end
-  end
 end
