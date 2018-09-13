33dd9db21cebbdf3fb5dd5a29b540e1c4fc19b5b
Lukegits <luket.chavez@gmail.com>
2018-09-12 20:13:17 -0600

    favorites added to posts and voting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,26 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   # #2
-     before_save { self.email = email.downcase if email.present? }
-     before_save { self.role ||= :member }
+  before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
-   # #3
-     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-   # #4
+  # #3
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  # #4
 
-    validates :password, length: { minimum: 6 }, allow_blank: true
-   # #5
-     validates :email,
-               presence: true,
-               uniqueness: { case_sensitive: false },
-               length: { minimum: 3, maximum: 254 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
+  # #5
+  validates :email,
+  presence: true,
+  uniqueness: { case_sensitive: false },
+  length: { minimum: 3, maximum: 254 }
 
-   # #6
-     has_secure_password
-      enum role: [:member, :admin]
+  # #6
+  has_secure_password
+  enum role: [:member, :admin]
+  def favorite_for(post)
+     favorites.where(post_id: post.id).first
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_011351) do
+ActiveRecord::Schema.define(version: 2018_09_13_020257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2018_09_13_011351) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_favorites_on_post_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -32,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_011351) do
     t.datetime "updated_at", null: false
     t.integer "topic_id"
     t.integer "user_id"
+    t.float "rank"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -65,6 +75,8 @@ ActiveRecord::Schema.define(version: 2018_09_13_011351) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "posts"
+  add_foreign_key "favorites", "users"
   add_foreign_key "votes", "posts"
   add_foreign_key "votes", "users"
 end
